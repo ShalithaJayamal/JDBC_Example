00dff11c873806ce7c0abc12e4b9185b5e72e4b5
ssss <shalithajayamal96@gmail.com>
2019-11-23 14:47:40 +0530

    createTable function

--- Main.java
+++ Main.java
@@ -2,15 +2,36 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		getConnection();
+		
+		createTable();
+		
 		
 		
 
+	}
+	public static void createTable() throws Exception
+	{
+		try {
+			//create prepared statement if not create table\co
+			
+			Connection con = getConnection();
+			PreparedStatement create = con.prepareStatement("create table staff(name varchar(25),department varchar(5),subject_name varchar(25))");
+			
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+			
+		}
+		finally {
+			System.out.println("Function is completed");
+		}
 	}
 	
 	public static Connection  getConnection() throws Exception
@@ -25,7 +46,7 @@ public class Main {
 			Connection conn = DriverManager.getConnection(url,username,password);
 			System.out.println("Connection is succesfull");
 			
-			//create prepared statement if not create table
+			
 			
 			
 		    
